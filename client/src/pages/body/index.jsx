c02eced1eb7b239e078eea2d5412ec4bf2019c18
Zeyad Amr <68791488+Zeyad-Amr@users.noreply.github.com>
2022-12-20 05:19:40 +0200

    layout

--- client/src/pages/body/index.jsx
+++ client/src/pages/body/index.jsx
@@ -1,13 +1,71 @@
 import { Container, Row, Col } from "react-bootstrap";
 import RecordingButton from "../../components/recording_btn";
-// import style from "./style.module.css";
+import style from "./style.module.css";
 
 const Body = () => {
   return (
-    <Container fluid>
-      <Row>
-        <Col>
-          <RecordingButton />
+    <Container fluid className={style.container}>
+      <Row fluid className={style.mainRow}>
+        <Col xs={4} className={style.recordePanel}>
+          <Row>
+            <RecordingButton />
+          </Row>
+        </Col>
+        <Col xs={8} className={style.repersenationContainer}>
+          <Row className={style.repersenationPanel}>
+            <Col className={style.repersenationSection}>
+              <h3>Headline</h3>
+              <li>
+                First Point: Lorem Ipsum is simply dummy text of the printing
+                and typesetting industry.
+              </li>
+              <li>
+                Second Point: Lorem Ipsum is simply dummy text of the printing
+                and typesetting industry.
+              </li>
+              <li>
+                Third Point: Lorem Ipsum is simply dummy text of the printing
+                and typesetting industry.
+              </li>
+              <li>
+                Forth Point: Lorem Ipsum is simply dummy text of the printing
+                and typesetting industry.
+              </li>
+            </Col>
+            <Col className={style.repersenationSection}>
+              <img
+                src="https://lightningchart.com/lightningchart-js-interactive-examples/images/spectrogram.png"
+                alt=""
+              />
+            </Col>
+          </Row>
+          <Row className={style.repersenationPanel}>
+            <Col className={style.repersenationSection}>
+              <h3>Headline</h3>
+              <li>
+                First Point: Lorem Ipsum is simply dummy text of the printing
+                and typesetting industry.
+              </li>
+              <li>
+                Second Point: Lorem Ipsum is simply dummy text of the printing
+                and typesetting industry.
+              </li>
+              <li>
+                Third Point: Lorem Ipsum is simply dummy text of the printing
+                and typesetting industry.
+              </li>
+              <li>
+                Forth Point: Lorem Ipsum is simply dummy text of the printing
+                and typesetting industry.
+              </li>
+            </Col>
+            <Col className={style.repersenationSection}>
+              <img
+                src="https://www.faberacoustical.com/wpblog/wp-content/uploads/2015/05/Spectrogram3D1024.png"
+                alt=""
+              />
+            </Col>
+          </Row>
         </Col>
       </Row>
     </Container>
